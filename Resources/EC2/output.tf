1beec176c3ed1537a9a3f46d19e5ca4b425ad1ee
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-19 12:34:55 +0530

    qwertty

--- Resources/EC2/output.tf
+++ Resources/EC2/output.tf
@@ -1,14 +1,14 @@
 output "ec2_instance_id" {
-  value       = aws_instance.ec2_instance.id
+  value       = aws_instance.web.id
   description = "The ID of the EC2 instance"
 }
 
 output "ec2_instance_public_ip" {
-  value       = aws_instance.ec2_instance.public_ip
+  value       = aws_instance.web.public_ip
   description = "The public IP address of the EC2 instance"
 }
 
 output "security_group_id" {
-  value       = aws_security_group.ec2_sg.id
+  value       = aws_security_group.this_student_sg.id
   description = "The ID of the security group for the EC2 instance"
 }
