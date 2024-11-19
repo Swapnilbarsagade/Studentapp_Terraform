d7832276d5eb717b314bd2f8112e725179612692
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-19 13:23:42 +0530

    qwerty

--- Resources/RDS/variable.tf
+++ Resources/RDS/variable.tf
@@ -15,12 +15,6 @@ variable "sg_name" {
   default     = "db-security-group"
 }
 
-variable "allowed_ssh_cidrs" {
-  description = "CIDR blocks allowed for SSH access"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]
-}
-
 variable "db_username" {
   description = "The master username for the RDS instance"
   type        = string
