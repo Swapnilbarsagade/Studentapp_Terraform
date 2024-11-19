72359cbd617af90f0a087b286e6870262cc92be9
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-19 12:55:10 +0530

    qwerty

--- Dev/module.tf
+++ Dev/module.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source               = "/home/cloudshell-user/Terraform/ModuleBlock/Resources/VPC"
+  source               = "/home/cloudshell-user/Studentapp_Terraform/Resources/VPC"
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidr   = "10.0.1.0/24"
   private_subnet_cidr  = "10.0.2.0/24"
@@ -13,7 +13,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source          = "/home/cloudshell-user/Terraform/ModuleBlock/Resources/EC2"
+  source          = "/home/cloudshell-user/Studentapp_Terraform/Resources/EC2"
   vpc_id          = module.vpc.vpc_id
   subnet_id       = module.vpc.public_subnet_id
   ami_id          = "ami-042e76978adeb8c48" # Replace with a valid AMI ID
