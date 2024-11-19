a4c79f033b081141438fa7a0fdf6f7e77beb3347
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-11-19 12:31:26 +0530

    qwerty

---
+++ Resources/EC2/variable.tf
@@ -0,0 +1,67 @@
+
+# Security Group
+variable "vpc_id" {
+  description = "The ID of the VPC where the security group will be created"
+  type        = string
+}
+
+variable "sg_name" {
+  description = "Name for the security group"
+  type        = string
+  default     = "ec2-security-group"
+}
+
+variable "allowed_ssh_cidrs" {
+  description = "CIDR blocks allowed for SSH access"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+# EC2 variables for studentapp: 
+
+variable "this_ami" {
+  description = "Type of the instance to launch"
+  type        = string
+  default     = "ami-042e76978adeb8c48"
+}
+
+variable "instance_type" {
+  description = "Type of the instance to launch"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Key pair name for the instance"
+  type        = string
+  default     = "batmobile"
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to associate a public IP address"
+  type        = bool
+  default     = true
+}
+
+variable "instance_count" {
+  description = "Number of instances to create"
+  type        = number
+  default     = 1
+}
+
+variable "availability_zone" {
+  description = "Availability zone to launch the instance"
+  type        = string
+  default     = "default"
+}
+
+variable "this_aws_instance_volume_size" {
+  description = "this_aws_instance_volume_size for instance"
+  type        = number
+  default     =  10
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet where the EC2 instance will be launched"
+  type        = string
+}
